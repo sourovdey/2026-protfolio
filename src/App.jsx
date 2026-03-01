3aa1be549cb441521170e2408bbc7ecfc9e64f73
sourovdey <developer.sourovdey@gmail.com>
2026-03-01 08:11:42 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Footer from './header_footer/footer'
-import Header from './header_footer/header'
-import Hero from './Section_Component/Hero'
+import Footer from './header_footer/footer.jsx'
+import Header from './header_footer/header.jsx'
+import Hero from './Section_Component/Hero.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
